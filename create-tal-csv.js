8563cd4eb9434ba423755c50ec1c219ee8518d1b
SciutoAlex <SciutoAlex@gmail.com>
2015-01-17 11:45:45 -0500

    Initial Commit
    Adding in files

---
+++ create-tal-csv.js
@@ -0,0 +1,110 @@
+var jsdom  = require('jsdom');
+var fs     = require('fs');
+var jquery = require('jquery');
+var cheerio = require('cheerio');
+var request = require('request');
+var csv = require('csv')
+
+//local variables
+var records = [['series_counter', 'episode_number', 'episode_date', 'episode_link', 'act_name', 'act_number', 'speaker_name', 'paragraph_number_per_speaker', 'time_stamp', 'content']];
+
+
+//Variables to control which shows to request. Check out thisamericalife.org to see latest episode number
+var maxEpisode = 544;
+var minEpisode = 0;
+var counter = minEpisode;
+
+
+
+//Begin the show
+next();
+function next() {
+  if(counter < maxEpisode) {
+    counter++;
+    fs.readFile(returnFileName(counter), 'utf8', function(err, body) {
+      if(!err) {
+        addHtmlToCsv(records, body, next);
+        console.log(counter);
+      } else {
+        next();
+
+      }
+    });
+  } else {
+    csv.stringify(records, function(err, data) {
+      if(!err) {
+        fs.writeFile('./output/data.csv', data);
+      } else {
+        console.log('something went wrong with the csv stringify');
+      }
+    })
+  }
+}
+
+
+function addHtmlToCsv(obj, html, cb) {
+  $ = cheerio.load(html);
+  var showCounter = 0;
+  var seriesCounter = obj.length;
+
+  var epNumb = getEpisodeNumber($('.radio-episode-num').html());
+  var dateString = getDateString($('.radio-date').html());
+  var episodeName = $('.radio h2 a').text();
+  var episodeLink = "http://www.thisamericanlife.org" + $('.radio h2 a').attr('href');
+
+  var acts = $('.act');
+  var actCounter = 0;
+  acts.each(function() {
+    var act = $(this);
+    var actName = act.find('h3').text();
+    var speakingParts = act.find('.subject, .interviewer, .host');
+    speakingParts.each(function() {
+      var speakingCounter = 0;
+      var speakingPart = $(this).attr('class');
+      var speaker = $(this).find('h4').text();
+      if(!speaker && speakingPart == "host") { speaker = "Ira Glass"; }
+      var paragraphs = $(this).find('p');
+      paragraphs.each(function() {
+        var time = $(this).attr('begin');
+        var content = $(this).text();
+        obj.push([
+            seriesCounter,
+            epNumb,
+            dateString,
+            episodeName,
+            episodeLink,
+            actName,
+            actCounter,
+            speaker,
+            speakingCounter,
+            time,
+            content
+        ]);
+        seriesCounter++;
+        speakingCounter++;
+      });
+
+    });
+    actCounter++;
+  });
+
+  cb();
+
+}
+function returnURL(number) {
+  var baseUrl = "http://www.thisamericanlife.org/radio-archives/episode/"+number+"/transcript";
+  return {url: baseUrl};
+}
+
+function returnFileName(number) {
+  return "./transcripts/show-"+number+".html";
+}
+
+function getEpisodeNumber(str) {
+  return str.substr(0, str.length-1);
+}
+
+function getDateString(str) {
+  str = str.substr(17, str.length);
+  return str;
+}
---
+++ get-html-files.js
@@ -0,0 +1,54 @@
+var fs     = require('fs');
+var request = require('request');
+
+//Variables to control the wait period between HTTP requests to TAL
+var time_base = 5000;
+var time_random = 10000;
+
+//Variables to control which shows to request. Check out thisamericalife.org to see latest episode number
+var maxEpisode = 544;
+var minEpisode = 0;
+
+
+
+//Begin the show
+next();
+function next() {
+  if(minEpisode < maxEpisode) {
+    minEpisode++;
+    if (!fs.existsSync(returnFileName(minEpisode))) {
+      setTimeout(function() {
+        request(returnURL(minEpisode), readTAL);
+      }, Math.random()*time_random + time_base);
+    } else {
+      console.log("skipped number: " + minEpisode);
+      next();
+    }
+  }
+}
+
+function readTAL(err, res, body) {
+  if (!err && res.statusCode == 200) {
+    fs.writeFile("./TAL/show-"+minEpisode+".html", body, function(err) {
+      if(err) {
+        console.log(err);
+        next();
+      } else {
+        console.log("Show "+minEpisode+" was saved!");
+        next();
+      }
+    });
+  } else {
+    console.log(err);
+    next();
+  }
+}
+
+function returnURL(number) {
+  var baseUrl = "http://www.thisamericanlife.org/radio-archives/episode/"+number+"/transcript";
+  return {url: baseUrl};
+}
+
+function returnFileName(number) {
+  return "./transcripts/show-"+number+".html";
+}
